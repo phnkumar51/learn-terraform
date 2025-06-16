93da69b3cb0955e9d00e5f868a09df807fa11236
Balaphani <phnkumar51@gmail.com>
2025-06-16 13:22:59 +0530

    Creating a file in our local

---
+++ sample-terra/var.tf
@@ -0,0 +1,17 @@
+# Normal Variable
+
+variable "x" {
+  default = 10
+}
+
+# output variable - To access variable - var.x ( for normal variables)
+
+output "x" {
+  value = var.x
+}
+
+# If a value is a combination of strings then to access a variable: "${var.x}"
+
+output "x1" {
+  value = "value of x - ${var.x}"
+}
