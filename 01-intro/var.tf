e42a80077d22c49a1724900fd84d18b141351a31
Balaphani <phnkumar51@gmail.com>
2025-06-16 13:28:40 +0530

    Creating a file in our local

---
+++ 01-intro/var.tf
@@ -0,0 +1,15 @@
+variable "x" {
+  default = 10
+}
+
+# output variable - To access variable - var.x ( for normal variables)
+
+output "x" {
+  value = var.x
+}
+
+# If a value is a combination of strings then to access a variable: "${var.x}"
+
+output "x1" {
+  value = "value of x - ${var.x}"
+}
