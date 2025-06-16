52c5f3ee883c61dc02e2e7dcaa71c5d0714bcbaa
Balaphani <phnkumar51@gmail.com>
2025-06-16 14:06:20 +0530

    Creating a file in our local

--- 01-intro/var.tf
+++ 01-intro/var.tf
@@ -2,16 +2,19 @@ variable "x" {
   default = 10
 }
 
+# List of variables
+variable "y" {
+  default = [ 1, "abc", 0.5, True]
+}
+
+
 # output variable - To access variable - var.x ( for normal variables)
 
 output "x" {
   value = var.x
 }
 
-# List of variables
-variable "y" {
-  default = [ 1, "abc", 0.5, True]
-}
+
 # If a value is a combination of strings then to access a variable: "${var.x}"
 
 output "x1" {
