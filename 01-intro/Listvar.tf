ec714c89d55347d1c3d6280c734030036d3578d6
Balaphani <phnkumar51@gmail.com>
2025-06-16 14:16:23 +0530

    Creating a file in our local

--- 01-intro/Listvar.tf
+++ 01-intro/Listvar.tf
@@ -1,8 +1,15 @@
 # List of variables
 variable "y" {
+  type = list(any)
   default = [ 1, "abc", 0.5, True]
 }
 
 output "y" {
   value = var.y[0]
 }
+
+
+# variable "y" {
+#  type    = list(any)
+#  default = [1, "abc", 0.5, true]
+# }
