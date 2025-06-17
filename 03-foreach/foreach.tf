4fff0d753fff64cfaa9b11808367ac03cc94ed35
Balaphani <phnkumar51@gmail.com>
2025-06-17 23:01:26 +0530

    asc

--- 03-foreach/foreach.tf
+++ 03-foreach/foreach.tf
@@ -1,7 +1,7 @@
-resource "resource_null" "test" {
+resource "null_resource" "test" {
   for_each = var.x
   provisioner "local-exec" {
-    command = " echo value of ${each.key} = ${each.value}"
+    command = "echo value of ${each.key} = ${each.value}"
   }
 }
 
