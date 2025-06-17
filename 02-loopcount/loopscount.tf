39d5e0f8ee0f755d82a014ac0fac66487139bd5a
Balaphani <phnkumar51@gmail.com>
2025-06-17 15:20:04 +0530

    Creating a file in our local

--- 02-loopcount/loopscount.tf
+++ 02-loopcount/loopscount.tf
@@ -1,5 +1,10 @@
-resource "null_resource" "dummy"{
+resource "null_resource" "dummy"
+{
   count = length(var.x)
+
+  provisioner "local_exec"{
+    command = "echo ${var.x[count.index]}"
+  }
 }
 
 variable "x" {
