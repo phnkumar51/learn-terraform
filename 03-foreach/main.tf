f00defaef430606bf81340deaaa761edbf8013fe
Balaphani <phnkumar51@gmail.com>
2025-06-17 22:56:03 +0530

    Creating a file in our local

---
+++ 03-foreach/main.tf
@@ -0,0 +1,13 @@
+resource "resource_null" "test" {
+  for_each = var.x
+  provisioner "local-exec" {
+    command = " echo value of ${each.key} = ${each.value}"
+  }
+}
+
+variable "x" {
+  default = {
+    i = 100
+    j = 200
+  }
+}
